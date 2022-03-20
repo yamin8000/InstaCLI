2414c394b2dea271fc5bef5731ffcae02a977791
YaMiN <yamin8000@yahoo.com>
2022-03-20 22:24:23 +0330

    doc added

--- src/main/kotlin/io/github/yamin8000/utils/Utility.kt
+++ src/main/kotlin/io/github/yamin8000/utils/Utility.kt
@@ -9,18 +9,33 @@ import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 import java.util.concurrent.CompletableFuture
 
+/**
+ * Utility class containing various utility functions
+ */
 object Utility {
 
+    /**
+     * returns the current time in ISO 8601 format
+     */
     fun now(): String = isoTime(LocalDateTime.now())
 
+    /**
+     * returns [String] form of [localDateTime] in ISO 8601 format
+     */
     fun isoTime(localDateTime: LocalDateTime): String {
         return DateTimeFormatter.ISO_DATE_TIME.format(localDateTime)
     }
 
+    /**
+     * returns [String] form of [epoch] in ISO 8601 format with [ZoneOffset.UTC]
+     */
     fun isoTimeOfEpoch(epoch: Long): String {
         return isoTime(LocalDateTime.ofEpochSecond(epoch, 0, ZoneOffset.UTC))
     }
 
+    /**
+     * returns [Dyad] instance containing [T]/[IGResponse] and [Exception] related to request for [T]
+     */
     fun <T : IGResponse> CompletableFuture<T>.pair(): Dyad<T?> {
         return try {
             val response = this.get()
@@ -31,6 +46,9 @@ object Utility {
         }
     }
 
+    /**
+     * returns [Dyad] instance containing [U]/[IGResponse] and [Exception] related to action for [U]
+     */
     fun <U> CompletableFuture<U>.actionPair(): Dyad<U?> {
         return try {
             val response = this.get()
@@ -40,12 +58,15 @@ object Utility {
         }
     }
 
+    /**
+     * @throws [IllegalArgumentException] if [limit] is lesser than 1
+     */
     fun requirePositiveLimit(limit: Int) {
         if (limit < 1) throw IllegalArgumentException("Limit must be greater than 0")
     }
 
     /**
-     * This method will inject non-null value to callback if it is not null,
+     * This method will notify the callback with a non-null value if it is not null,
      * otherwise,
      * If data is null error message is shown to user.
      *
