b4d92cc2eadfba7aa10269bac8912cbdc7d6018c
YaMiN <yamin8000@yahoo.com>
2023-02-07 06:05:20 +0330

    updated jackson

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     application
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("java")
-    id("org.jetbrains.dokka") version "1.7.10"
 }
 
 group = "io.github.instakiller"
@@ -19,7 +18,8 @@ repositories {
 dependencies {
     implementation("com.github.instagram4j:instagram4j:2.0.7")
     implementation("com.github.ajalt.mordant:mordant:2.0.0-beta7")
-
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.14.2")
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.10")
     implementation("org.slf4j:slf4j-simple:2.0.4")
 }
@@ -30,10 +30,6 @@ application {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
-}
-
-tasks.dokkaJekyll.configure {
-    outputDirectory.set(projectDir.resolve("docs"))
 }
 
 tasks.jar {
